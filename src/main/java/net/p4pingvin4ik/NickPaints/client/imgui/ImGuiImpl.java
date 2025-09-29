f738f6441e42661565e224bcd8c0b51e048e9d06
maksim <msakunov@inbox.ru>
2025-09-29 18:13:18 +0300

    font size change

--- src/main/java/net/p4pingvin4ik/NickPaints/client/imgui/ImGuiImpl.java
+++ src/main/java/net/p4pingvin4ik/NickPaints/client/imgui/ImGuiImpl.java
@@ -32,7 +32,7 @@ import static org.lwjgl.opengl.GL11C.*;
 public class ImGuiImpl {
     private final static ImGuiImplGlfw imGuiImplGlfw = new ImGuiImplGlfw();
     private final static ImGuiImplGl3 imGuiImplGl3 = new ImGuiImplGl3();
-    private static final float FONT_SIZE_PIXELS = 18.0f;
+    private static final float FONT_SIZE_PIXELS = 20.0f;
     private static int gFontTexture = -1;
     public static void create(final long handle) {
         ImGui.createContext();
